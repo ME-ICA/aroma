fc5f9d408b0b5516b5cb6825251ca215a7bc2b2c
Vicente Ferrer <v.ferrer@bcbl.eu>
2020-11-11 12:32:25 +0100

    Update aroma/_version.py
    Co-authored-by: Eneko Uruñuela <13706448+eurunuela@users.noreply.github.com>

--- aroma/_version.py
+++ aroma/_version.py
@@ -203,7 +203,7 @@ def git_versions_from_keywords(keywords, tag_prefix, verbose):
         if ref.startswith(tag_prefix):
             r = ref[len(tag_prefix):]
             if verbose:
-                LGR.info("picking %s" % r)
+                LGR.info("Picking %s" % r)
             return {"version": r,
                     "full-revisionid": keywords["full"].strip(),
                     "dirty": False, "error": None,
