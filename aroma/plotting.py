04b08e692a0165b9dae2c3810cc50d7681dcca9a
vinferrer <v.ferrer@bcbl.eu>
2020-11-11 15:39:25 +0100

    correct plotting messages

--- aroma/plotting.py
+++ aroma/plotting.py
@@ -24,7 +24,7 @@ def classification_plot(myinput, out_dir):
     """
     # find files
     myfiles = glob.glob(myinput)
-    LGR.info('Found', len(myfiles), 'file(s)')
+    LGR.info(f'Found {len(myfiles)} file(s)')
 
     # load in data from files
     count = 0
@@ -53,8 +53,8 @@ def classification_plot(myinput, out_dir):
     # get counts
     ncomp = len(df)
     nmot = len(df.loc[df[0] == "True"])
-    LGR.info('Found', nmot, 'head motion-related components in a total of',
-             ncomp, 'components.')
+    LGR.info(f'Found {nmot} head motion-related components in a total of',
+             f'{ncomp} components.')
 
     # add dummy components if needed,
     # this is just for making the plots look nice
