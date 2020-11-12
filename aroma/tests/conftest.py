37eb62052a7b9576085880dcb764754d82d220e6
Eneko <enekouru@gmail.com>
2020-11-12 11:32:49 +0100

    Redo unit test for feature tests and add data to compare with

--- aroma/tests/conftest.py
+++ aroma/tests/conftest.py
@@ -1,8 +1,10 @@
-import os
+import os.path as op
 import pytest
 
 from nilearn.datasets import fetch_development_fmri
 
+
+import aroma
 
 def pytest_addoption(parser):
     parser.addoption(
@@ -25,5 +27,35 @@ def testpath(tmp_path_factory):
 
 
 @pytest.fixture
+def featurespath():
+    return op.join(op.dirname(aroma.__file__), 'tests', 'data', 'features_test')
+
+
+@pytest.fixture
 def nilearn_data(testpath):
     return fetch_development_fmri(n_subjects=1, age_group="adult")
+
+
+@pytest.fixture
+def mel_FT_mix(featurespath):
+    return op.join(featurespath, 'melodic_FTmix')
+
+
+@pytest.fixture
+def mel_mix(featurespath):
+    return op.join(featurespath, 'melodic_mix')
+
+
+@pytest.fixture
+def mc(featurespath):
+    return op.join(featurespath, 'mc.tsv')
+
+
+@pytest.fixture
+def mel_IC(featurespath):
+    return op.join(featurespath, 'melodic_IC_thr_MNI2mm.nii.gz')
+
+
+@pytest.fixture
+def features_df(featurespath):
+    return op.join(featurespath, 'features.csv')
--- aroma/tests/test_features.py
+++ aroma/tests/test_features.py
@@ -1,3 +1,4 @@
+from aroma.tests.conftest import features_df
 import numpy as np
 import os.path as op
 import pandas as pd
@@ -8,43 +9,35 @@ from aroma import features
 from aroma.tests.utils import get_tests_resource_path
 
 
-def test_feature_time_series(nilearn_data):
-
-    # Get path to confounds file
-    test_path, _ = op.split(nilearn_data.func[0])
-
-    # Read confounds
-    confounds = pd.read_csv(nilearn_data.confounds[0], sep="\t")
-
-    # Extract motion parameters from confounds
-    mc = confounds[["rot_x", "rot_y", "rot_z", "trans_x", "trans_y", "trans_z"]]
-    mc_path = op.join(test_path, "mc.tsv")
-    mc.to_csv(mc_path, sep="\t", index=False, header=None)
-
-    # Get path to mel_mix file
-    cwd = get_tests_resource_path()
-    mel_mix = op.join(cwd, "melodic_mix")
+def test_feature_time_series(mel_mix, mc, features_df):
 
     # Run feature_time_series
-    max_RP_corr = features.feature_time_series(mel_mix, mc_path)
+    max_RP_corr = features.feature_time_series(mel_mix, mc)
 
-    # Expected values
-    true_max_RP_corr = np.array(
-        [0.65255575, 0.86003032, 0.88690363, 0.61399576, 0.43840624]
-    )
+    # Read features csv
+    df = pd.read_csv(features_df)
 
-    assert np.allclose(max_RP_corr[: len(true_max_RP_corr)], true_max_RP_corr, atol=1e-2)
+    assert np.allclose(df["max_RP_corr"].values[:5], max_RP_corr[:5], atol=1e-1)
 
 
-def test_feature_frequency(nilearn_data):
+def test_feature_frequency(mel_FT_mix, features_df):
 
-    # Get path to mel_mix file
-    cwd = get_tests_resource_path()
-    mel_T_mix = op.join(cwd, "melodic_FTmix")
+    # Run feature_frequency
+    HFC = features.feature_frequency(mel_FT_mix, TR=2)
 
-    HFC = features.feature_frequency(mel_T_mix, TR=2)
+    # Read features csv
+    df = pd.read_csv(features_df)
 
-    # Expected values
-    true_HFC = np.array([0.96279762, 0.08234127, 0.13194444, 0.96279762, 0.04513889])
+    assert np.allclose(df["HFC"].values, HFC, atol=1e-2)
 
-    assert np.allclose(HFC[: len(true_HFC)], true_HFC, atol=1e-2)
+
+def test_feature_spatial(mel_IC, features_df):
+
+    # Run feature_spatial
+    edge_fract, csf_fract = features.feature_spatial(mel_IC)
+
+    # Read features csv
+    df = pd.read_csv(features_df)
+
+    assert np.allclose(df["edge_fract"].values, edge_fract, atol=1e-2)
+    assert np.allclose(df["csf_fract"].values, csf_fract, atol=1e-2)
