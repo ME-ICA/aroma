e8de46698b291103357c761d2faf1f63fb722dc1
Taylor Salo <tsalo006@fiu.edu>
2021-09-16 13:43:54 -0400

    Update test_integration.py

--- aroma/tests/test_integration.py
+++ aroma/tests/test_integration.py
@@ -47,8 +47,6 @@ def test_integration(skip_integration, nilearn_data):
     assert isfile(join(out_path, "classified_motion_ICs.txt"))
     assert isfile(join(out_path, "denoised_func_data_nonaggr.nii.gz"))
     assert isfile(join(out_path, "feature_scores.tsv"))
-    assert isfile(join(out_path, "melodic_IC_thr.nii.gz"))
-    assert isfile(join(out_path, "melodic_IC_thr_MNI2mm.nii.gz"))
 
     # Check classification overview file
     true_classification_overview = pd.read_csv(join(resources_path, "classification_overview.txt"),
